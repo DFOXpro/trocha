004cc14874292cdc54a8699aafc82c22e04a1340
dfoxpro <dfoxpro@gmail.com>
2016-08-22 12:33:48 -0500

    Fix missing _constructor variable

--- dist/trocha.js
+++ dist/trocha.js
@@ -59,8 +59,91 @@ this.trocha = (function() {
   _basicResource[_list][$ + ID] = false;
   _basicResource[_list][$ + HIDE] = true;
   trochaReturn = function(initParams) {
-    var _preparePath, as, newResource, newRoute, newScope, routes;
+    var _constructor, _preparePath, _prepareRoutes, as, newResource, newRoute, newScope, routes;
     routes = {};
+    _constructor = function(initParams) {
+      if (initParams[DOMAIN]) {
+        routes[$domain] = s + initParams[DOMAIN];
+      }
+      if (initParams[PREFIX]) {
+        routes[$prefix] = s + initParams[PREFIX];
+      }
+      if (initParams[POSTFIX]) {
+        routes[$postfix] = s + initParams[POSTFIX];
+      }
+      if (initParams[RESOURCE.toLowerCase()]) {
+        routes[$resource] = initParams[RESOURCE.toLowerCase()];
+      }
+      if (initParams[CUSTOM_SELECTOR]) {
+        routes[$customSelector] = initParams[CUSTOM_SELECTOR];
+      }
+      routes[NEW_SCOPE] = newScope;
+      routes[NEW_ROUTE] = newRoute;
+      routes[NEW_RESOURCE] = newResource;
+      routes[CUSTOM] = function(param, routeParam) {
+        return _preparePath({}, param)(routeParam);
+      };
+      if (initParams[ROUTES]) {
+        _prepareRoutes(routes, initParams[ROUTES]);
+      }
+      return routes;
+    };
+    _prepareRoutes = function(parent, routesJSON, SELECTOR) {
+      var _$, posibleRoutes;
+      _$ = SELECTOR || routes[$customSelector] || $;
+      delete routesJSON[_$ + TYPE];
+      posibleRoutes = Object.keys(routesJSON);
+      return posibleRoutes.forEach(function(name) {
+        var newResourceParam, newRouteParam, newScopeParam, route;
+        route = routesJSON[name];
+        if (typeof route !== 'object') {
+          console.error('Did you mean', _$ + name, '? Route definition must be Object');
+          throw 'TrochaJS error: [_prepareRoutes] invalid route definition. name = ' + name;
+        }
+        if (route[_$ + TYPE] === SCOPE) {
+          newScopeParam = {};
+          newScopeParam[NAME] = name;
+          if (route[_$ + ID] !== void 0) {
+            newScopeParam[ID] = route[_$ + ID];
+            delete route[_$ + ID];
+          }
+          parent[NEW_SCOPE](newScopeParam);
+        } else if (route[_$ + TYPE] === RESOURCE) {
+          newResourceParam = {};
+          newResourceParam[NAME] = name;
+          if (route[_$ + ID] !== void 0) {
+            newResourceParam[ID] = route[_$ + ID];
+            delete route[_$ + ID];
+          }
+          parent[NEW_RESOURCE](newResourceParam);
+        } else {
+          newRouteParam = {};
+          newRouteParam[NAME] = name;
+          if (route[_$ + ID] !== void 0) {
+            newRouteParam[ID] = route[_$ + ID];
+            delete route[_$ + ID];
+          }
+          if (route[_$ + METHOD]) {
+            newRouteParam[METHOD] = route[_$ + METHOD];
+            delete route[_$ + METHOD];
+          }
+          if (route[_$ + JUST_ID]) {
+            newRouteParam[JUST_ID] = route[_$ + JUST_ID];
+            delete route[_$ + JUST_ID];
+          }
+          if (route[_$ + AFTER_ID]) {
+            newRouteParam[AFTER_ID] = route[_$ + AFTER_ID];
+            delete route[_$ + AFTER_ID];
+          }
+          if (route[_$ + HIDE]) {
+            newRouteParam[HIDE] = route[_$ + HIDE];
+            delete route[_$ + HIDE];
+          }
+          parent[NEW_ROUTE](newRouteParam);
+        }
+        return _prepareRoutes(parent[name], route);
+      });
+    };
     _preparePath = function(parent, param) {
       return function(routeParams) {
         var fragment, hide, noIdentifier, query, r;
